8c07682ea7e54d72b95838ece47d2dd7001da1f7
eden <noedenkim@gmail.com>
2025-06-14 23:03:28 +0900

    first commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by 김이든 on 6/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+
+}
+
+
+
+// MARK: - ViewController Preview
+
+#if DEBUG
+
+@available(iOS 17.0, *)
+#Preview {
+  ViewController()
+}
+
+#endif
